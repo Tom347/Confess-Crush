7265b0ea5b9a145234e10722be8f9f8aede2859a
Tom347 <97297036+Tom347@users.noreply.github.com>
2022-01-07 21:40:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "I love you",
-    introTitle: '1 ai đó',
+    introTitle: 'Người tôi yêu',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -16,5 +16,5 @@ const CONFIG = {
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Trưa mai 12h, mình chơi game nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/profile.php?id=100058221316890' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
